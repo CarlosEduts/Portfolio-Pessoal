c942c3b39298996d0f8d6948595af5613d0244be
Carlos Eduardo <cedut.dev@gmail.com>
2024-10-09 11:41:08 +0000

    feat(blog): Adição do link para o meu blog pessoal, e correção no tema de cores

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-const userPrefersDark = window.matchMedia(
-  "(prefers-color-scheme: dark)"
-).matches;
-
-if (userPrefersDark) {
-  document.body.classList.remove("light-theme");
-} else {
-  document.body.classList.add("light-theme");
-}
