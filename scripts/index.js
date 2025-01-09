14c7dc342327e63d60ac6e9acc2b55b246790c83
Carlos Eduardo <131536369+CarlosEduts@users.noreply.github.com>
2025-01-09 18:37:48 +0000

    feat(header): Acição de uma header superior para melhorar navegação

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,26 @@
-const toggleButton = document.getElementById('theme-toggle');
-const toggleSvg = document.querySelector(".theme-toggle-svg")
-let svgRotate = 0
+const toggleButton = document.getElementById("theme-toggle");
+const toggleSvg = document.querySelector(".theme-toggle-svg");
+let svgRotate = 0;
+
+// Verificar preferencia de cor do usuário guardada no localStorage
+let localIsLightMode = window.localStorage.getItem("dark-mode");
+if (localIsLightMode === "true") {
+  localIsLightMode = true;
+} else {
+  localIsLightMode = false;
+}
+document.documentElement.classList.toggle("dark-mode", !localIsLightMode);
+document.documentElement.classList.toggle("light-mode", localIsLightMode);
 
 // Alterna o tema manualmente
-toggleButton.addEventListener('click', () => {
-  const isDarkMode = document.documentElement.classList.contains('dark-mode');
-  document.documentElement.classList.toggle('dark-mode', !isDarkMode);
-  document.documentElement.classList.toggle('light-mode', isDarkMode);
-  svgRotate += 180
-  console.log(svgRotate)
-  toggleSvg.style.transform = `rotate(${svgRotate}deg)`
+toggleButton.addEventListener("click", () => {
+  const isLightMode = document.documentElement.classList.contains("dark-mode");
+  document.documentElement.classList.toggle("dark-mode", !isLightMode);
+  document.documentElement.classList.toggle("light-mode", isLightMode);
+
+  window.localStorage.setItem("dark-mode", isLightMode);
+
+  // Rotacionar botão (Animação)
+  svgRotate += 180;
+  toggleSvg.style.transform = `rotate(${svgRotate}deg)`;
 });
